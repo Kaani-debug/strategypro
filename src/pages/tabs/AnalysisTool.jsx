8a2d47d18a45095f340fba1a4a908a19b045b747
User <user@strategypro.dev>
2026-08-06 15:14:31 +0300

    Published build c7d4b1a (routing fix + warm theme + smartcharts)

--- src/pages/tabs/AnalysisTool.jsx
+++
@@ -1,5 +0,0 @@
-import AnalysisWorkspace from '../../components/workspace/AnalysisWorkspace'
-
-export default function AnalysisTool() {
-  return <AnalysisWorkspace />
-}
--- start.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-cd /d "C:\Users\user\OneDrive\Documents\Default Project\strategypro"
-node serve.cjs
